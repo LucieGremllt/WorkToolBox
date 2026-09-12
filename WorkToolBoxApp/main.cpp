23fa36ce1ce61c53c883f6b9542797251e7020ac
LucieGrmlt <luciegremllt@gmail.com>
2026-09-12 13:53:20 +0200

    erreur de définition window dans le main

--- WorkToolBoxApp/main.cpp
+++ WorkToolBoxApp/main.cpp
@@ -6,15 +6,10 @@
 int main(int argc, char *argv[])
 {
     QApplication app(argc, argv);
-
     Window window;
-    QWidget window;
-    window.setGeometry(0,0,1400, 600);
+    window.setGeometry(0, 0, 1400, 600);
 
-    QPushButton button("Calculator", &window);
-    button.setText("Calculator");
-    button.setToolTip("A tooltip");
-    button.setGeometry(10, 10, 200, 40);
+    
     window.show();
 
     return app.exec();
